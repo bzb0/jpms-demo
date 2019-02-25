5ddda6da82aec387fa748c8036ae96b1b348b587
Bogdan Z <bzb0@proton.me>
2019-02-25 20:52:35 +0100

    Added an example, where the JPMS will detect a split-package problem, when two auto-named modules are the same jar.

---
+++ auto-modules/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module com.zbit.module.auto {
+  requires commons.math3;
+  /* The inclusion of the following line will fail,
+     since there will be a split-package problem */
+  // requires evil.math;
+}
