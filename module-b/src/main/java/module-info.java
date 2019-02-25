5bc62f009efcc91240f67da785d09e378a6908fc
Bogdan Zafirov <bogdan.zafirov@yahoo.com>
2019-02-25 18:48:45 +0100

    Added a maven dependency (commons-lang3-3.5), which was converted to an automatic module. This is an example for the JPMS directive 'requires transitive'.

--- module-b/src/main/java/module-info.java
+++ module-b/src/main/java/module-info.java
@@ -1,4 +1,6 @@
 module com.zbit.module.b {
     exports com.zbit.strings.util;
     requires java.base;
+    /* This is an automatic module, derived by Java. */
+    requires /* transitive */ commons.lang3;
 }
