e127b07f562d07416afc647718d69e2b3b0fe3c6
Bogdan Zafirov <bogdan.zafirov@yahoo.com>
2019-02-25 20:37:04 +0100

    Created a simple example, which depicts the usage of the JPMS directive 'opens'.

---
+++ module-b/src/main/java/com/zbit/reflective/ReflectiveClass.java
@@ -0,0 +1,10 @@
+package com.zbit.reflective;
+
+public class ReflectiveClass {
+
+  private static final int SIZE = 10;
+
+  public String getSomething() {
+    return "something";
+  }
+}
--- module-b/src/main/java/module-info.java
+++ module-b/src/main/java/module-info.java
@@ -1,6 +1,11 @@
 module com.zbit.module.b {
     exports com.zbit.strings.util;
     requires java.base;
+
     /* This is an automatic module, derived by Java. */
+    /* The referenced JAR file has to have a named that will be derived to 'commons.lang3'. */
     requires /* transitive */ commons.lang3;
+
+    /* Opening a package for usage at runtime. */
+    opens com.zbit.reflective;
 }
