68430099990c25d0f64db8988a979524850a032a
Bogdan Zafirov <bogdan.zafirov@yahoo.com>
2019-04-29 21:03:24 +0200

    Fixed formatting and removed obsolete lines in pom.xml files. Refactored Java classes.

--- auto-modules/src/main/java/module-info.java
+++ auto-modules/src/main/java/module-info.java
@@ -1,6 +1,5 @@
 module com.zbit.module.auto {
   requires commons.math3;
-  /* The inclusion of the following line will fail,
-     since there will be a split-package problem */
+  /* The inclusion of the following line will fail, since there will be a split-package problem */
   // requires evil.math;
 }
--- module-a/src/main/java/com/zbit/modules/base/ComplexNumberConsumer.java
+++ module-a/src/main/java/com/zbit/modules/base/ComplexNumberConsumer.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.zbit.modules.base;
 
 import java.lang.reflect.Method;
@@ -11,9 +26,7 @@ public class ComplexNumberConsumer {
   public void printComplexNumber(Complex complex) {
     Complex moduleComplex = new Complex(5, 2);
     System.out.println(moduleComplex.add(complex));
-
-    System.out.println("Modulepath classloader: "
-        + moduleComplex.getClass().getClassLoader().getName());
+    System.out.println("Modulepath classloader: " + moduleComplex.getClass().getClassLoader().getName());
   }
 
   public Complex getComplexNumber() {
@@ -22,12 +35,10 @@ public class ComplexNumberConsumer {
       Method m = c.getMethod("createComplexNumber", double.class);
 
       Object instance = c.getDeclaredConstructor().newInstance();
-      System.out.println("Classpath classloader: "
-          + instance.getClass().getClassLoader().getName());
+      System.out.println("Classpath classloader: " + instance.getClass().getClassLoader().getName());
 
       return (Complex) m.invoke(instance, random.nextDouble());
     } catch (Throwable e) {
-      System.err.println(e);
       throw new IllegalStateException("Reflection problem.");
     }
   }
